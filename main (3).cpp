8bbbb35f29e311d3c310a6e7e2b63a5b48a6c237
roaa hiasat <roaaalheasa09t@gmail.com>
2026-01-09 20:24:05 +0300

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+    //هي عباره عن جمل تكراريه مثل طباعه اسم 100 مره loop
+   
+   //loops: for/while/do while  
+   
+   
+   for(int i=1;i<=100;)
+   {
+        cout<<i<<": Roaa\n";
+        i++;
+   }
+   
+   
+   
+   
+    return 0;
+}
