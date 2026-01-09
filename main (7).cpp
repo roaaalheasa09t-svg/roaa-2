88ac3e18180b5751fdc30ede53f764bf68096277
roaa hiasat <roaaalheasa09t@gmail.com>
2026-01-09 22:17:42 +0300

    Add files via upload

---
+++ main (7).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+     
+ /*  int x=1;
+   while(x<=5)
+ {
+      cout<<x<<endl;
+      x++;
+ }*/
+ 
+                         //(do while)
+/* int x=1;
+ do //جمله تكرار
+ 
+ {
+    cout<<x<<endl;
+    x++;
+ }while(x>=5);
+ //هون الشرط ما بتحقق لانه 1 لا تساوي او اكبر من 5
+ // فقطdoاذن تنفذ جمله 
+
+*/
+
+
+ int x=1;
+ do //جمله تكرار
+ 
+ {
+    cout<<x<<endl;
+    x++;
+ }while(x<=5);
+ //هون الشرط متحقق  رح يطبع من 1-5
+
+
+
+
+
+
+
+
+
+    return 0;
+}
