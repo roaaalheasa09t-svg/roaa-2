988bbae9d7050aa1a365b1489ea26a426d709ffb
roaa hiasat <roaaalheasa09t@gmail.com>
2026-01-09 20:49:49 +0300

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+   /* for(int i=1;i<=10;)
+    {
+         i++;// coutهون بتبلش من 2 -11 لانها قبل 
+      cout<<i<<":roaa\n";
+
+    }*/
+    
+    
+    for(int i=10;i>=1;)//هي الي ببدا منها الرقم العدint
+    //(بدي يطبعلي تنازلي من 10-1
+    {
+     cout<<i<<":roaa\n";
+     i--;
+    }
+
+   
+   
+   
+    return 0;
+}
