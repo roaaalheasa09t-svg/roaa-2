d29077b799cd82d8bce3eb9888492273b959569c
roaa hiasat <roaaalheasa09t@gmail.com>
2026-01-09 19:02:45 +0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std ;
+int main()
+{
+  int x=10;
+   switch(x)
+   {
+        case 3:
+      cout<<"the 3\n";
+      
+      case 10:
+      cout<<"the 10 \n";
+      break;
+      
+      case 2:
+      cout<<"the 2\n";
+      
+      default:
+      cout<<"no num";
+   
+   
+   
+   
+   
+   
+   
+    return 0;
+}
